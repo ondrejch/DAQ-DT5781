284909d79af439394f664b3a5e71a5df5298770c
Ondrej Chvala <ondrejch@gmail.com>
2015-06-14 22:40:12 +0200

    Initial commit

---
+++ spigs/src/spigs.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : spigs.cpp
+ Author      : Ondrej Chvala <ochvala@utk.edu>
+ Version     :
+ Copyright   : GNU/GPL
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
