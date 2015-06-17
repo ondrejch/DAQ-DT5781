0d9190902e306b281ae9c19efdcc144327211ed6
Ondrej Chvala <ondrejch@gmail.com>
2015-06-17 23:38:50 +0200

    ROOT works even in Eclipse!

--- spigs/Debug/src/subdir.mk
+++ spigs/Debug/src/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include/root -I"/home/o/workspace/git/DAQ-DT5781/spigs/src" -I/usr/local/include/CAEN -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I/usr/include/root -I"/home/o/workspace/git/DAQ-DT5781/spigs/src" -I/usr/local/include/CAEN -O3 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
