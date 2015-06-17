82e8e816c329a0fc0cfe4533f3c2e9d86c331afc
Ondrej Chvala <ondrejch@gmail.com>
2015-06-17 21:27:12 +0200

    builds

--- spigs/Debug/src/newDAQ.d
+++
@@ -1,6 +0,0 @@
-src/newDAQ.d: ../src/newDAQ.cpp /home/o/workspace/newDAQ/src/PigsDAQ.h \
- /usr/local/include/CAEN/CAENDPPLib.h
-
-/home/o/workspace/newDAQ/src/PigsDAQ.h:
-
-/usr/local/include/CAEN/CAENDPPLib.h:
--- spigs/Debug/src/subdir.mk
+++ spigs/Debug/src/subdir.mk
@@ -6,24 +6,24 @@
 CPP_SRCS += \
 ../src/PigsDAQ.cpp \
 ../src/PigsDAQ_test.cpp \
-../src/newDAQ.cpp 
+../src/spigs.cpp 
 
 OBJS += \
 ./src/PigsDAQ.o \
 ./src/PigsDAQ_test.o \
-./src/newDAQ.o 
+./src/spigs.o 
 
 CPP_DEPS += \
 ./src/PigsDAQ.d \
 ./src/PigsDAQ_test.d \
-./src/newDAQ.d 
+./src/spigs.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include/root -I"/home/o/workspace/newDAQ/src" -I/usr/local/include/CAEN -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I/usr/include/root -I"/home/o/workspace/git/DAQ-DT5781/spigs/src" -I/usr/local/include/CAEN -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
